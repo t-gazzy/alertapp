b4a9e347ddab5d6fc42abc7a47648e0584aeb998
t-gazzy <t.kaji@ntt.com>
2023-06-09 23:23:02 +0900

    first commit

---
+++ AlertApp/AlertApp/AlertAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertAppApp.swift
+//  AlertApp
+//
+//  Created by takemasa kaji on 2023/06/09.
+//
+
+import SwiftUI
+
+@main
+struct AlertAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlertApp/AlertApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AlertApp
+//
+//  Created by takemasa kaji on 2023/06/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
