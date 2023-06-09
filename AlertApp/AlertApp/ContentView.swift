05b76cb0ce2d3811b38e87e56fb1cab76605104a
t-gazzy <t.kaji@ntt.com>
2023-06-09 23:37:34 +0900

    feat: impl ui design

--- AlertApp/AlertApp/ContentView.swift
+++ AlertApp/AlertApp/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var text = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("入力してください", text: $text)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            Text("分後に通知します")
+            Button("設定", action: {
+                
+            })
+            .padding()
+            .accentColor(Color.white)
+            .background(Color.blue)
+            .cornerRadius(.infinity)
         }
         .padding()
     }
